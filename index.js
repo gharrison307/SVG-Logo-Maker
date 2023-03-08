730149328dd12a829fc31eaa95648f70ebe61195
George Harrison <g.a.harrison91@gmail.com>
2023-03-08 09:46:52 -0700

    corrected question text. Questions now running correctly in command line

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-inquirer.prompt([
-  {
-    type: "input",
-    message: "Please enter three charcters for your text?",
-    name: "letters",
-  },
-  {
-    type: "input",
-    message: "Please enter a text color (color keyword or hexadecimal).",
-    name: "textColor",
-  },
-  {
-    type: "input",
-    message: "Please choose a shape.",
-    name: "installInstructions",
-  },
-  {
-    type: "list",
-    message: "Please enter the usage information.",
-    name: "shape",
-    choices: ["circle", "triangle", "square"],
-  },
-  {
-    type: "input",
-    message: "Please enter a shape color (color keyword or hexadecimal).",
-    name: "shapeColor",
-  },
-]);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Please enter three charcters for your text.",
+      name: "letters",
+    },
+    {
+      type: "input",
+      message: "Please enter a text color (color keyword or hexadecimal).",
+      name: "textColor",
+    },
+    {
+      type: "list",
+      message: "Please pick a shape.",
+      name: "shape",
+      choices: ["circle", "triangle", "square"],
+    },
+    {
+      type: "input",
+      message: "Please enter a shape color (color keyword or hexadecimal).",
+      name: "shapeColor",
+    },
+  ])
+  .then((data) => {
+    fs.writeFile("../SVGLogo.html", createFile(data), (error) => {
+      error ? console.log("error") : console.log("creating your SVG logo now.");
+    });
+  });
